9e9b10389140d7cefda77d184ef404ba8b9ad071
Conrado Cañas <conradocanas@gmail.com>
2020-12-08 11:15:31 -0300

    First upload

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <DashBoard></DashBoard>
+  </v-container>
+</template>
+
+<script>
+import DashBoard from "./Dashboard"
+
+// @ is an alias to /src
+export default {
+  name: "Home",
+  components: {
+    DashBoard
+  },
+  data() {
+    return {
+      series: [],
+    }
+  },
+  mounted() {
+    fetch("http://164.90.149.113:3200/api/dolares/grafico/oficial/7")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    });
+  }
+}
+</script>
