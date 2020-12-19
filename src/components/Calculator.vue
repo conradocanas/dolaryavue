f8941b511c06d5a193ceee44fac98f0d2b466fce
NazgulFS <nahuelsalto.71@gmail.com>
2020-12-19 19:44:35 -0300

    calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -40,25 +40,24 @@
           >Seleccioná el tipo de transacción:</v-col
         >
         <v-col cols="12" class="d-flex justify-center pt-0">
-          <v-btn
-            small
-            class="primary mr-2"
-            @click="total('compra')"
-            >Compra</v-btn
-          >
-          <v-btn
-            small
-            v-model="promedio"
-            class="primary mr-2"
-            @click="total('promedio')"
-            >Promedio</v-btn
-          >
-          <v-btn
-            small
-            class="primary"
-            @click="total('venta')"
-            >Venta</v-btn
-          >
+            <v-btn
+              small
+              class="primary mr-2"
+              @click="activeCurrencies = 'compra'"
+              >Compra</v-btn
+            >
+            <v-btn
+              small
+              class="primary mr-2"
+               @click="activeCurrencies = 'promedio'"
+              >Promedio</v-btn
+            >
+            <v-btn
+              small
+              class="primary"
+              @click="activeCurrencies = 'venta'"
+              >Venta</v-btn
+            >
         </v-col>
       </v-row>
       <div class="pa-4">
@@ -66,13 +65,15 @@
           dense
           label="Dolares (USD)"
           outlined
+          @keyup="totalArs(activeCurrencies)"
           v-model="selectedUsdCurrency"
         ></v-text-field>
         <v-text-field
           dense
           label="Pesos Argentinos (ARS)"
+          @keyup="totalUsd(activeCurrencies)"
           outlined
-          v-model="calculatorValue"
+          v-model="selectedArsCurrency"
         ></v-text-field>
       </div>
     </v-card-text>
@@ -97,14 +98,25 @@ export default {
       selectedArsCurrency: 1,
       selectedUsdCurrency: 1,
       selectedTransaction: "sell",
-      calculatorValue: 0,
-      promedio: 1
+      promedio: 1,
+      firstInputSelected: true,
+      activeCurrencies: '',
     };
   },
   mounted: function() {
     this.readDataFromAPI();
   },
+  watch: {
+    activeCurrencies: function() {
+      console.log(this.activeCurrencies);
+      this.totalArs(this.activeCurrencies);
+      this.totalUsd(this.activeCurrencies);
+    },
+  },
   methods: {
+    floatParse(elem) {
+      return parseFloat(elem.replace(",", ".").replace(" ", ""));
+    },
     //Reading data from API BlockChain.info
     readDataFromAPI() {
       this.loading = true;
@@ -119,65 +131,63 @@ export default {
         }
       });
     },
-    calcInput_1: function(e, rate) {
-      this.firstInputSelected = true;
-      this.calculate(e, rate);
-    },
-    calcInput_2: function(e, rate) {
-      this.firstInputSelected = false;
-      this.calculate(e, rate);
-    },
-    updateInputs: function() {
-      var selected;
-      this.APIvalues.forEach((elem) => {
-        if (this.selected == elem.name) {
-          selected = elem;
-        }
-      });
-      this.countryRate = selected.last;
-
-      var input2 = parseFloat(document.getElementById("currencyInput").value);
-      if (isNaN(input2)) {
-        this.calc2 = "";
-        this.calc1 = "";
-        return;
-      }
-      this.calc1 = (input2 * this.countryRate).toFixed(2);
-    },
-    calculate: function(e, value) {
-      value = parseFloat(e.target.value);
-      if (isNaN(value)) {
-        this.calc2 = "";
-        this.calc1 = "";
-        return;
-      }
-
-      if (this.firstInputSelected) {
-        this.calc2 = value;
-        this.calc1 = (value * this.countryRate).toFixed(2);
+    totalArs: function(transactType) {
+      console.log(transactType);
+      if (this.selectedUsdCurrency == "" || this.selectedUsdCurrency == 0) {
+        this.selectedArsCurrency = 0;
       } else {
-        this.calc1 = value;
-        this.calc2 = (value / this.countryRate).toFixed(5);
+        if (transactType === "promedio") {
+          let average =
+            (this.floatParse(this.$store.state.calculatedItem.buy) +
+              this.floatParse(this.$store.state.calculatedItem.sell)) /
+            2;
+          this.selectedArsCurrency = (
+            parseFloat(this.selectedUsdCurrency) * average
+          ).toFixed(2);
+        }
+        if (transactType === "compra") {
+          this.selectedArsCurrency = (
+            this.floatParse(this.selectedUsdCurrency) *
+            this.floatParse(this.$store.state.calculatedItem.buy)
+          ).toFixed(2);
+        }
+        if (transactType === "venta") {
+          this.selectedArsCurrency = (
+            this.floatParse(this.selectedUsdCurrency) *
+            this.floatParse(this.$store.state.calculatedItem.sell)
+          ).toFixed(2);
+        }
       }
     },
-    total: function(transactType) {
-      if(transactType == 'promedio') {
-        let average = (this.$store.state.calculatedItem.compra+this.$store.state.calculatedItem.venta)/2;
-        this.calculatorValue = this.selectedUsdCurrency * average;
-        return console.log(this.$store.state.calculatedItem.venta)
+    totalUsd: function(transactType) {
+      console.log(this.activeCurrencies);
+      if (this.selectedArsCurrency == "" || this.selectedArsCurrency == 0) {
+        this.selectedUsdCurrency = 0;
+      } else {
+        if (transactType == "promedio") {
+          let average =
+            (this.floatParse(this.$store.state.calculatedItem.buy) +
+              this.floatParse(this.$store.state.calculatedItem.sell)) /
+            2;
+          this.selectedUsdCurrency = (
+            parseFloat(this.selectedArsCurrency) / average
+          ).toFixed(2);
+        }
+        if (transactType == "compra") {
+          this.selectedUsdCurrency = (
+            this.floatParse(this.selectedArsCurrency) /
+            this.floatParse(this.$store.state.calculatedItem.buy)
+          ).toFixed(2);
+        }
+        if (transactType == "venta") {
+          this.selectedUsdCurrency = (
+            this.floatParse(this.selectedArsCurrency) /
+            this.floatParse(this.$store.state.calculatedItem.sell)
+          ).toFixed(2);
+        }
       }
-      if(transactType == 'compra') {
-        this.calculatorValue = this.selectedUsdCurrency * this.$store.state.calculatedItem.compra;
-        return console.log(this.calculatorValue)
-      }
-      if(transactType == 'venta') {
-        this.calculatorValue = this.selectedUsdCurrency * this.$store.state.calculatedItem.venta;
-        return console.log(this.calculatorValue)
-      }
-    }
+    },
   },
-    computed: mapState([
-    "calculatedItem"
-  ]),
+  computed: mapState(["calculatedItem"]),
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: true,
   state: {
+    /* activeCurrencies: 'oficial', */
     currencies: defaultPair,
     tickers: {},
     chartData: [],
