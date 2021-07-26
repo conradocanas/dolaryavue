a806c94af7270555a607ac7e5129fcbfcb5cbd21
Conrado Cañas <conradocanas@gmail.com>
2021-07-26 12:20:53 -0300

    [MENU] Tooltip

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,13 +5,28 @@
         <v-col class="d-flex justify-left align-center">
           <h2>DolarYa.ar</h2>
           <span class="d-flex align-end ml-1">
-            <img style="max-height: 20px; padding-top: 8px" src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg" alt="">
+            <img
+              style="max-height: 20px; padding-top: 8px"
+              src="https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg"
+              alt=""
+            />
           </span>
         </v-col>
         <v-col class="d-flex justify-end align-center">
-          <v-btn small @click="$emit('drawerStatus')" text>
-            <v-icon>mdi-menu</v-icon>
-          </v-btn>
+          <v-tooltip color="primary" v-model="tooltip" bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                small
+                @click="$emit('drawerStatus')"
+                text
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-menu</v-icon>
+              </v-btn>
+            </template>
+            <span style="font-size: 12px">Personaliza tu panel</span>
+          </v-tooltip>
         </v-col>
       </v-row>
     </v-container>
@@ -21,10 +36,12 @@
 <script>
 export default {
   data: () => ({
-    drawer: false
+    drawer: false,
+    tooltip: true
   }),
   created() {
     this.$vuetify.theme.dark = true;
-  },
+    setTimeout(() => { this.tooltip = false}, 3000)
+  }
 };
 </script>
